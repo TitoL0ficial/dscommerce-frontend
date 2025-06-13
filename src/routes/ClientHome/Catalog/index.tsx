c58d598bae502c9707a1e3142940d1912a06fbd7
Tito0ficial <titol.oficial@gmail.com>
2025-06-13 19:23:03 -0300

    URL params and Custom behavior

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,29 +2,7 @@ import './styles.css';
 import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import type { ProductDTO } from '../../../models/product';
-
-const product: ProductDTO = {
-    id: 2,
-    name: "Smart TV",
-    description: "TV full HD 75 polegadas Samsung",
-    imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
-    price: 2500.99,
-    categories: [
-        {
-            id: 2,
-            name: "Eletrônicos"  
-        },
-        {
-            id: 3,
-            name: "Computadores"  
-        },
-        {
-            id: 4,
-            name: "Importados"  
-        }
-    ]
-}
+import * as productService from '../../../services/product-service';
 
 export default function Catalog() {
 
@@ -33,16 +11,11 @@ export default function Catalog() {
             <section id="catalog-section" className="dsc-container">
                 <SearchBar/>
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-                   <CatalogCard product={product}/> 
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>
-                   <CatalogCard product={product}/>                
+                    {
+                        productService.findAll().map(
+                            product => <CatalogCard key={product.id} product={product}/>
+                        )
+                    }              
                 </div>     
                 <ButtonNextPage/>      
             </section>
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,35 +2,23 @@ import './styles.css';
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import type { ProductDTO } from '../../../models/product';
+import * as productService from "../../../services/product-service";
+import { useParams } from 'react-router-dom';
 
-const product: ProductDTO = {
-    id: 2,
-    name: "Smart TV",
-    description: "TV full HD 75 polegadas Samsung",
-    imgUrl: "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
-    price: 2500.99,
-    categories: [
-        {
-            id: 2,
-            name: "Eletrônicos"  
-        },
-        {
-            id: 3,
-            name: "Computadores"  
-        },
-        {
-            id: 4,
-            name: "Importados"  
-        }
-    ]
-}
 
 export default function ProductDetails () {
+
+    const params = useParams();
+
+    const product = productService.findById(Number(params.productId));
+
     return (
         <main>
             <section id="product-details-section" className="dsc-container">
-            <ProductDetailsCard product={product}/>
+            {
+                product &&
+                <ProductDetailsCard product={product}/>
+            }           
             <div className="dsc-btn-page-container">
                 <ButtonPrimary text="Comprar"/>
                 <ButtonInverse text="Início"/>
