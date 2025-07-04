e48fe8e0cfcb7e7048e8a8a8eb7574696bd64f91
Tito0ficial <titol.oficial@gmail.com>
2025-07-04 19:41:52 -0300

    useEffect Catalog, params productId

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -15,7 +15,7 @@ export default function CatalogCard({ product } : Props) {
                 </div>
                 <div className="dsc-catalog-card-bottom">
                     <h3>R$ {product.price.toFixed(2)}</h3>
-                    <h4>{product.description}</h4>
+                    <h4>{product.name}</h4>
                 </div>
             </div>
         </Link>
