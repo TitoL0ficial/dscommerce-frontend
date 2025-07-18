19cda0a1e5046f5a51e45d99970d26f46994c295
Tito0ficial <titol.oficial@gmail.com>
2025-07-18 10:18:53 -0300

    Cart save/get

---
+++ src/services/cart-service.ts
@@ -0,0 +1,10 @@
+import type { OrderDTO } from "../models/order";
+import * as cartRepository from "../localStorage/cart-repository";
+
+export function saveCart(cart: OrderDTO) {
+    cartRepository.save(cart);
+}
+
+export function getCart() : OrderDTO {
+    return cartRepository.get();
+}
