cfaad54e5379bf6a3a46df25e77406443952b713
Tito0ficial <titol.oficial@gmail.com>
2025-07-29 19:31:46 -0300

    Searchbar onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,13 +1,35 @@
 import './styles.css';
 import searchButton from '../../assets/search-button.svg';
+import { useState } from 'react';
 
-export default function SearchBar() {
+type Props = {
+    onSearch: Function;
+}
+
+export default function SearchBar({ onSearch } : Props) {
+
+    const [text, setText] = useState("");
+
+    function handleChange(event: any) {
+        setText(event.target.value);
+    }
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+        onSearch(text);
+    }
+
     return (
         <div className="dsc-search-bar">
-            <form className="dsc-form-search dsc-container">
-                <input type="text" placeholder="Nome do produto" />
+            <form className="dsc-form-search dsc-container" onSubmit={handleSubmit}>
+                <input 
+                    value={text}
+                    type="text" 
+                    placeholder="Nome do produto" 
+                    onChange={handleChange}
+                />
                 <button className="dsc-button-search" type="submit">
-                <img src={searchButton} alt="icon" />
+                    <img src={searchButton} alt="icon" />
                 </button>
             </form>
         </div>
