e0fb24f4c8dfa5b1b46116e66c7d12a181bd47c5
Tito0ficial <titol.oficial@gmail.com>
2025-08-19 10:06:52 -0300

    hasAnyRoles

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,5 +1,5 @@
 import QueryString from "qs";
-import type { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
+import type { AccessTokenPayloadDTO, CredentialsDTO, RoleEnum } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import type { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
@@ -55,5 +55,23 @@ export function isAuthenticated(): boolean {
     if (tokenPayload && tokenPayload.exp * 1000 > Date.now()) {
         return true;
     }
+	return false;
+}
+
+export function hasAnyRoles(roles: RoleEnum[]): boolean {
+	if (roles.length === 0) {
+		return true;
+	}
+	
+	const tokenPayload = getAccessTokenPayload();
+	
+	if (tokenPayload !== undefined) {
+		for (var i = 0; i < roles.length; i++) {
+			if (tokenPayload.authorities.includes(roles[i])) {
+				return true;
+			}
+		}
+		//return roles.some(role => tokenData.authorities.includes(role));
+	}
 	return false;
 }
